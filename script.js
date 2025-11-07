b92975158293f504e5e531d1480921f198f5b498
awikner <awikner1@umd.edu>
2025-11-07 10:32:48 -0600

    Udpated pixel distance to 30 pixels

--- script.js
+++ script.js
@@ -453,7 +453,7 @@ function updateMarkersByYear(fitMap = true) {
     
     // Cluster markers that are close together (within 50 pixels at current zoom level)
     // Using pixel distance so clustering adapts to zoom level
-    const clusters = clusterMarkers(allMarkers, map, 50);
+    const clusters = clusterMarkers(allMarkers, map, 30);
     
     console.log(`Created ${clusters.length} clusters from ${allMarkers.length} markers`);
     let clusterCount = 0;
